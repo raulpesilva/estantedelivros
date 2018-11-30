24485638947777908b0530802beabd5d8d0dd3d8
raulpesilva <adraulpesilva@gmail.com>
2018-11-30 20:16:33 -0200

    add pesquisa and jquery

--- js/book.js
+++ js/book.js
@@ -3,6 +3,50 @@ function handleResponse(response) {
         var item = response.items[i];
         // in production code, item.text should have the HTML entities escaped.
         var livro = document.getElementById("thumb").innerHTML += "<img class=\"thumb-livro\" src=" + item.volumeInfo.imageLinks.thumbnail + ">";
-        console.log(livro)
+        // console.log(livro);
     }
 }
+
+function plotPesquisa(response) {
+    for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        // in production code, item.text should have the HTML entities escaped.
+        var livro = document.getElementById("thumb").innerHTML += "<img class=\"thumb-livro\" src=" + item.volumeInfo.imageLinks.thumbnail + ">";
+        // console.log(livro);
+    }
+
+}
+
+// jquery script
+$("#caixa-pesquisa").keyup(function (event) {
+    if ($("#caixa-pesquisa").val() === "") {
+
+    } else {
+        if (event.keyCode === 13) {
+            $("#botao-pesquisa").click();
+        }
+    }
+});
+
+$("#botao-pesquisa").click(function () {
+    event.preventDefault();
+    if ($("#caixa-pesquisa").val() === "") {
+
+    } else {
+        document.getElementById("thumb").innerHTML = ""
+        var inputLivro = $(".texto-pesquisa").val();
+        // console.log(inputLivro);
+        var linkPesquisa = "https://www.googleapis.com/books/v1/volumes?q=" + inputLivro;
+        $.ajax({
+            url: linkPesquisa,
+            type: "get",
+            dataType: "json",
+            success: function (response) {
+                plotPesquisa(response);
+            },
+            error: function (erro) {
+                console.log(erro);
+            }
+        });
+    }
+});
